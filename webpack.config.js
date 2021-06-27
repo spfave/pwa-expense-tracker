a6ef040b87242d5f79ebb488b64eb10cecb2e506
Sebastian F <sebpfave2@gmail.com>
2021-06-27 00:27:25 -0400

    Add babel rules to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,20 @@ const config = {
     path: path.join(__dirname, 'public', 'dist'),
     filename: path.join('assets', 'js', '[name].bundle.js'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
   plugins: [
     new WebpackPwaManifest({
       filename: 'manifest.json',
