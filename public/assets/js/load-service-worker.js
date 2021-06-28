1a4d4c19b2c0ae53406db2741a7e9744e8cec55a
Sebastian F <sebpfave2@gmail.com>
2021-06-28 00:05:35 -0400

    Refactor service worker load to async await

--- public/assets/js/load-service-worker.js
+++ public/assets/js/load-service-worker.js
@@ -1,17 +1,20 @@
 // Functions
-function loadServiceWorker() {
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-      .register('./service-worker.js', {
-        scope: '/',
-      })
-      .then((res) => {
-        console.log(res);
-        console.log('Service Worker registered successfully.');
-      })
-      .catch((error) =>
-        console.log('Service Worker registration failed:', error)
-      );
+async function loadServiceWorker() {
+  // Check if browser supports service workers
+  if (!('serviceWorker' in navigator)) {
+    return alert(
+      'Browser does not support service workers.\nOffline functionality will not work.'
+    );
+  }
+
+  // Try to register service worker
+  try {
+    const res = await navigator.serviceWorker.register('./service-worker.js', {
+      scope: '/',
+    });
+    console.log('Service Worker registered successfully.');
+  } catch (error) {
+    console.log('Service Worker registration failed:', error);
   }
 }
 
