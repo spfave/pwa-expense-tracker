be22afabccb2036c1f0317261c3fa9308f06c2df
Sebastian F <sebpfave2@gmail.com>
2021-06-28 21:47:41 -0400

    Set webpack build to production

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 
 const config = {
-  mode: 'development',
+  mode: 'production',
   entry: {
     index: '/public/assets/js/index.js',
   },
